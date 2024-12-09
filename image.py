c453d1667eb00b477896ba54b93202114070df4b
SAKURA <65160691+SAGARI-YU@users.noreply.github.com>
2024-12-09 20:52:29 +0800

    2024-12-29-20-52

--- image.py
+++ image.py
@@ -1,23 +1,39 @@
 import requests
 from lxml import etree
-
-def image(url,headers):
-    pass
-    
-# def image_url():
-#     response=requests.get(base_url,headers=headers)
-#     res=etree.HTML(response.text,'lxml')
-#     url=res.xpath('//dl[@class="egeli_pic_dl"]//a/@href')
-#     return url
-
-if __name__ == "__main__":
-    base_url="https://www.enterdesk.com/zhuomianbizhi"
-    headers={
-        "cookie":"t=5756b273e2ac119003f306f847b59c1d; r=6102; Hm_lvt_86200d30c9967d7eda64933a74748bac=1733733802; HMACCOUNT=2677EC9453ECBF9C; Hm_lpvt_86200d30c9967d7eda64933a74748bac=1733733934",
-        "user-agent":"Mozilla/5.0, (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/127.0.0.0 Safari/537.36 Edg/127.0.0.0"
-        }
-    response=requests.get("https://www.enterdesk.com/bizhi/64549.html",headers=headers)
+import os
+def get_content(url,headers):
+    response=requests.get(url,headers=headers)
     res=etree.HTML(response.text)
     url=res.xpath('//div[@class="swiper-wrapper"]//a/@src')
     title=res.xpath('//div[@id="images_show_zoom"]//a/@title')
-    print(title)
+    return url,title
+    
+def image_url(url,headers):
+    response=requests.get(url,headers=headers)
+    res=etree.HTML(response.text)
+    url=res.xpath('//dl[@class="egeli_pic_dl"]//a/@href')
+    return url
+
+def write_content(url,headers):
+    links,titles=get_content(url,headers)
+    title=titles[0]
+    if not os.path.exists("E:\\skill leaner\\pythoncode\\class1\\图片"):
+        os.chdir("E:\\skill leaner\\pythoncode\\class1")
+        os.mkdir(f"//图片//{title}")
+    for num,link in enumerate(links):
+         num +=1
+         pic_content=requests.get(link,headers=headers).content
+         with open(f"//图片//{title}//{title}{num}.jpg","wb") as f:
+              f.write(pic_content)
+              print(f"已下载....{title}..编号..{num}")
+
+if __name__ == "__main__":
+        headers={
+        "cookie":"Hm_lvt_86200d30c9967d7eda64933a74748bac=1733744278; HMACCOUNT=35E8DAA113C64D64; t=10f9355e14d6dc2f18b8ff614526fdc6; r=8989; Hm_lpvt_86200d30c9967d7eda64933a74748bac=1733744970",
+        "user-agent":"Mozilla/5.0, (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/127.0.0.0 Safari/537.36 Edg/127.0.0.0"
+        }
+        for i in range(1,10):
+            base_url=f"https://www.enterdesk.com/zhuomianbizhi/{i}.html"
+            url_list=image_url(base_url,headers)
+            for url in url_list:
+                write_content(url,headers)
