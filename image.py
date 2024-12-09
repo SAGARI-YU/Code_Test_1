0027ce08ade7db1b6525561f5e0cd7715c700728
SAKURA <65160691+SAGARI-YU@users.noreply.github.com>
2024-12-09 17:11:53 +0800

    2024-12-9-17-12

--- image.py
+++ image.py
@@ -1,23 +1,23 @@
 import requests
-from   hyper.contrib import HTTPConnection
-def image_list(url,headers):
-    response=requests.get(f'{url}+"4kmeinv/"',headers=headers)
-    
-def image_url():
+from lxml import etree
+
+def image(url,headers):
     pass
+    
+# def image_url():
+#     response=requests.get(base_url,headers=headers)
+#     res=etree.HTML(response.text,'lxml')
+#     url=res.xpath('//dl[@class="egeli_pic_dl"]//a/@href')
+#     return url
 
 if __name__ == "__main__":
-    base_url="https://pic.netbian.com"
+    base_url="https://www.enterdesk.com/zhuomianbizhi"
     headers={
-        ":authority":"pic.netbian.com",
-        "referer":"https://pic.netbian.com/4k/index_61.html",
-        "upgrade-insecure-requests":"1",
+        "cookie":"t=5756b273e2ac119003f306f847b59c1d; r=6102; Hm_lvt_86200d30c9967d7eda64933a74748bac=1733733802; HMACCOUNT=2677EC9453ECBF9C; Hm_lpvt_86200d30c9967d7eda64933a74748bac=1733733934",
         "user-agent":"Mozilla/5.0, (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/127.0.0.0 Safari/537.36 Edg/127.0.0.0"
         }
-with  HTTPConnection(host="https://pic.netbian.com/",secure=True) as con:
-    response=con.request("GET","/4kmeinv/",headers=headers)
-    rsp=con.get_response(response)
-    rsp_code=rsp.status()
-    rsp_text=rsp.read().decode()
-    print(rsp_code)
-    print(rsp_text)
+    response=requests.get("https://www.enterdesk.com/bizhi/64549.html",headers=headers)
+    res=etree.HTML(response.text)
+    url=res.xpath('//div[@class="swiper-wrapper"]//a/@src')
+    title=res.xpath('//div[@id="images_show_zoom"]//a/@title')
+    print(title)
