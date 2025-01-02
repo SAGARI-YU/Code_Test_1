ef4d86dac7a8c7ae23fedd137257e39b133f822b
SAKURA <65160691+SAGARI-YU@users.noreply.github.com>
2025-01-02 17:17:18 +0800

    2025年1月2日17点17分

--- server.py
+++ server.py
@@ -7,22 +7,32 @@ app=Flask(__name__)
 
 @app.route('/')
 def index():
-    city =101160901
-    data=get_weather(city)
-    # location='天津'
-    # temp='1'
-    # desc='小雨'
+    # city =101160901
+    # data=get_weather(city)
+    location='天津'
+    temp='1'
+    desc='小雨'
     # with open('./templates/index.html','r',encoding='utf-8') as f:
     #     content=f.read()
-    return render_template('index.html',location=data['name'],temp=data['temp'],desc=data['desc'])
-    # return render_template('index.html',location=location,temp=temp,desc=desc)
+    # return render_template('index.html',location=data['name'],temp=data['temp'],desc=data['desc'])
+    return render_template('index.html',location=location,temp=temp,desc=desc)
     # return content
 
 @app.route('/get/data')
 def weather_data():
-    msg='请求正常'
     loc=request.args.get('loc')
-    weather=get_weather(loc)
+    ua=request.headers.get('User-Agent')
+    cookie=request.cookies.get('cookie')
+    weather='NO DATA'
+    if 'python' in ua:
+        msg='检测到自动化程序'
+    elif not cookie or cookie != 'abc':
+        msg='cookie错误'
+    else:
+        msg='请求正常'
+        weather=get_weather(loc)
+    # msg = '请求正常'
+    # weather=get_weather(loc)
     weather_info={'msg':msg,'data':weather}
     return json.dumps(weather_info)
 def get_weather(city_code):
--- 爬虫.py
+++ 爬虫.py
@@ -1,9 +1,14 @@
 import requests
 
-def get_weather():
-    url='http://127.0.0.1:8655/get/data?loc=101160701'
-    r=requests.get(url)
-    print(r.json())
-
+def get_weather(city=None):
+    headers={
+        'user-agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/131.0.0.0 Safari/537.36 Edg/131.0.0.0'
+    }
+    cookie={
+        'cookie': 'abc'
+    }
+    url=f'http://127.0.0.1:8655/get/data?loc={city}'
+    r=requests.get(url,headers=headers,cookies=cookie)
+    print(r.text)
 if __name__=='__main__':
     get_weather()
